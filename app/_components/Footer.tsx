c5a4af41a29a64d802c0a95c3f2339578c227f38
theobeck <theodor.beck11@gmail.com>
2024-09-29 00:37:04 +0200

    feat: :sparkles: Created Footer and put it in root layout file

---
+++ app/_components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="h-16 w-full z-30 absolute bottom-0 py-1 px-4 bg-p-light-blue text-p-dark-blue flex items-center">
+        <div>Takk for at du besøkte siden min!</div>
+      </footer>
+    </>
+  );
+}
